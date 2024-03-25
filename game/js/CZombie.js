2efbaf4209a3740729271c011f21380c28bc496b
robin <116967343+ROBlNET13@users.noreply.github.com>
2024-03-25 16:19:33 +0200

    Update CZombie.js

--- game/js/CZombie.js
+++ game/js/CZombie.js
@@ -1541,7 +1541,7 @@ var CZombies = (function (b, a) {
   }),
   oZombie = InheritO(OrnNoneZombies, {
     EName: "oZombie",
-    CName: "Zombie",
+    CName: "普通僵尸",
     StandGif: 9,
     PicArr: (function () {
       var a = "images/Zombies/Zombie/";
@@ -5364,7 +5364,7 @@ var CZombies = (function (b, a) {
   })),
   (oCZombie = InheritO(OrnNoneZombies, {
     EName: "oCZombie",
-    CName: "Zombie",
+    CName: "庶民僵尸",
     StandGif: 9,
     PicArr: (function () {
       var a = "images/Zombies/wall/Zombie/";
