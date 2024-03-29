e5ae495af95d094777f0c9a33c44a63c0721e5f6
ClaytonTDM <claytontdm@gmail.com>
2024-03-29 11:26:16 -0400

    refactored cheat keybindings

--- game/js/cheats.js
+++ game/js/cheats.js
@@ -1,32 +1,27 @@
-// when j is pressed, let the user spawn a sun at a random location
-document.addEventListener("keydown", function (event) {
-    if (event.keyCode == 74) {
-        AppearSun(
-            GetX(Math.floor(1 + Math.random() * oS.C)),
-            GetY(Math.floor(1 + Math.random() * oS.R)),
-            25,
-            1
-        );
-    }
-});
-// when k is pressed, show handbook (almanac)
-document.addEventListener("keydown", function (event) {
-    if (event.keyCode == 75) {
-        ViewHandBook();
-    }
-});
-// when b, l, and n are pressed down at the same time, spawn a balloon
 let keysPressed = {};
 
 document.addEventListener('keydown', event => {
   keysPressed[event.key] = true;
-  
+  // balloon
   if (keysPressed['b'] && keysPressed['l'] && keysPressed['n']) {
     oP.Balloon();
   }
-  // next level
-  if (keysPressed['n'] && keysPressed['l']) {
+  // restart
+  if (keysPressed['r']) {
     document.querySelector('#btnNextLevel').click();
+  }
+  // almanac (handbook)
+  if (keysPressed('k')) {
+    ViewHandBook();
+  }
+  // sun
+  if (keysPressed['j']) {
+    AppearSun(
+        GetX(Math.floor(1 + Math.random() * oS.C)),
+        GetY(Math.floor(1 + Math.random() * oS.R)),
+        25,
+        1
+    );
   }
 });
 
