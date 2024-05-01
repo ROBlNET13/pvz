750c93fa590fa23f1638b0403c8fb2a926e0bdf2
robin <116967343+ROBlNET13@users.noreply.github.com>
2024-05-01 23:41:29 +0300

    ice fumeshroom

--- game/js/CPlants.js
+++ game/js/CPlants.js
@@ -4069,21 +4069,37 @@ if (yuckrng == 1) {
             );
         },
     }),
-    oIceFumeShroom = InheritO(oFumeShroom, {
+        oIceFumeShroom = InheritO(oFumeShroom, {
         EName: "oIceFumeShroom",
-        CName: "寒冰大喷菇",
+        CName: "Icy Fume-shroom",
+        SunNum: 200,
         PicArr: [
-            "images/Card/Plants/FumeShroom.png",
-            "images/Plants/FumeShroom/0.gif",
-            "images/Plants/FumeShroom/FumeShroom.gif",
-            "images/Plants/FumeShroom/FumeShroomSleep.gif",
-            "images/Plants/FumeShroom/FumeShroomAttack.gif",
-            "images/Plants/FumeShroom/FumeShroomBullet.gif",
+            "images/Card/Plants/IcyFumeShroom.png",
+            "images/Plants/IcyFumeShroom/0.gif",
+            "images/Plants/IcyFumeShroom/FumeShroom.gif",
+            "images/Plants/IcyFumeShroom/FumeShroomSleep.gif",
+            "images/Plants/IcyFumeShroom/FumeShroomAttack.gif",
+            "images/Plants/IcyFumeShroom/FumeShroomBullet.gif",
         ],
-        Tooltip: "喷射可以穿过门板的气液",
+        Tooltip: "Shoots icy fumes that can pass through screen doors",
         Produce:
             '大喷菇喷出的臭气可以穿透铁丝网门。<p>Harm:<font color="#FF0000">普通，可穿透铁丝网门</font><br>Scope:<font color="#FF0000">臭气中的所有僵尸<br>白天睡觉</font></p>“我以前那份没前途的工作，是为一个面包房</font><br>生产酵母孢，”大喷菇说。“然后小喷菇，上帝</font><br>保佑它，告诉了我这个喷杀僵尸的机会。现在</font><br>我真觉得自己完全不同了。”',
-        NormalAttack: function () {
+            PrivateBirth: function (b) {
+                var a = b.id;
+                NewEle(
+                    a + "_Bullet",
+                    "div",
+                    "position:absolute;visibility:hidden;width:343px;height:62px;left:" +
+                        b.AttackedRX +
+                        "px;top:" +
+                        (b.pixelTop + 5) +
+                        "px;background:url(images/Plants/IcyFumeShroom/FumeShroomBullet.gif);z-index:" +
+                        (b.zIndex + 1),
+                    0,
+                    EDPZ
+                );
+            },
+            NormalAttack: function () {
             PlayAudio("fume");
             var f = this,
                 d = oZ.getArZ(
@@ -4100,7 +4116,7 @@ if (yuckrng == 1) {
                 (g = d[e]).Altitude < 2 && g.getSnowPea(g, 20);
             }
             b.childNodes[1].src =
-                "images/Plants/FumeShroom/FumeShroomAttack.gif";
+                "images/Plants/IcyFumeShroom/FumeShroomAttack.gif";
             SetVisible($(a));
             ImgSpriter(
                 a,
@@ -4120,7 +4136,7 @@ if (yuckrng == 1) {
                     var h = $(j);
                     $P[j] &&
                         ((h.childNodes[1].src =
-                            "images/Plants/FumeShroom/FumeShroom.gif"),
+                            "images/Plants/IcyFumeShroom/FumeShroom.gif"),
                         SetHidden($(i)));
                 }
             );
