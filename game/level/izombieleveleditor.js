6c796de9beb3f3a2d97653f9c27d227c90ed418a
ClaytonTDM <claytontdm@gmail.com>
2024-10-07 22:12:53 -0400

    fix border
    Signed-off-by: ClaytonTDM <claytontdm@gmail.com>

--- game/level/izombieleveleditor.js
+++ game/level/izombieleveleditor.js
@@ -253,17 +253,15 @@
                                         $("dAll").appendChild(levelDataElement);
                                         const copyButtonElement = document.createElement("button");
                                         copyButtonElement.style.position = "absolute";
-                                        copyButtonElement.style.left = "calc(75% - 17px)";
+                                        copyButtonElement.style.left = "calc(75% - 20px)";
                                         copyButtonElement.style.top = "50%";
                                         copyButtonElement.style.transform = "translate(-50%, -50%)";
                                         copyButtonElement.style.width = "35px";
                                         copyButtonElement.style.height = "34px";
                                         copyButtonElement.style.cursor = "pointer";
                                         copyButtonElement.style.backgroundColor = "#fff";
-                                        copyButtonElement.style.borderStyle = "hidden solid hidden hidden";
+                                        copyButtonElement.style.borderStyle = "hidden";
                                         copyButtonElement.style.borderRadius = "0px 10px 10px 0px";
-                                        copyButtonElement.style.borderColor = "#767676";
-                                        copyButtonElement.style.borderWidth = "2px";
                                         copyButtonElement.style.zIndex = "1000";
                                         copyButtonElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M0 0h24v24H0z" stroke="none"/><path d="M7 9.667A2.667 2.667 0 0 1 9.667 7h8.666A2.667 2.667 0 0 1 21 9.667v8.666A2.667 2.667 0 0 1 18.333 21H9.667A2.667 2.667 0 0 1 7 18.333z"/><path d="M4.012 16.737A2 2 0 0 1 3 15V5c0-1.1.9-2 2-2h10c.75 0 1.158.385 1.5 1"/></svg>`;
                                         copyButtonElement.onclick = function () {
