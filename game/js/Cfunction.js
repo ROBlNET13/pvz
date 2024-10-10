eb26d0d2ee77ecf59c88f57a6b943da91bffdb42
ClaytonTDM <ClaytonTDM@users.noreply.github.com>
2024-10-10 19:15:57 +0000

    Prettified Code!

--- game/js/Cfunction.js
+++ game/js/Cfunction.js
@@ -919,7 +919,10 @@ var $User = (function () {
 			// console.log(image.parentElement);
 		},
 		AddZombiesFlag: function (d) {
-			if (Math.floor(Math.random() * 5) == 1 && $("dSunNum").style.visibility == "") {
+			if (
+				Math.floor(Math.random() * 5) == 1 &&
+				$("dSunNum").style.visibility == ""
+			) {
 				oP.Balloon();
 			}
 			var g = oP,
