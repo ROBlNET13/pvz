493ee082efbe0b4ae04ea29f9c53bb1a626ef8a4
ROBlNET13 <robert.robinet13@gmail.com>
2024-12-14 22:46:59 +0200

    added zombie fail popup for future update and a commit hash on the main menu
    Co-authored-by: ClaytonTDM <claytontdm@clickette.net>

--- game/js/Custom.js
+++ game/js/Custom.js
@@ -1,3 +1,29 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+// wait for #commit to exist
+waitForElm('#commit').then(elm => {
+	// set the innertext to that of the contents of ../v.txt
+	fetch('../v.txt').then(response => response.text()).then(text => elm.innerText = text);
+});
+
 const saveWhitelist = [
 	1,
 	2,
