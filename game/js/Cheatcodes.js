c71d0aa4d6353036b53c72d22c71642db43adf43
ROBlNET13 <robert.robinet13@gmail.com>
2026-01-18 04:30:27 +0200

    removed useless stuff from selectmodals in vasebreaker and izombie menu and made cheatcode code better

--- game/js/Cheatcodes.js
+++ game/js/Cheatcodes.js
@@ -1,6 +1,19 @@
 let keySequence = "";
 let sequenceTimeout = null;
 
+let cheatCodes = {
+	"speed": () => { CSpeed(1000, 10, 1000); },
+	"balloon": () => { oP.Balloon(); },
+	"restartlevel": () => { SelectModal(oS.Lvl); },
+	"handbook": () => { ViewHandBook(); },
+	"j": () => {
+		const sunId = AppearSun($User.Mouse.x - 45, $User.Mouse.y + 45, 25, false);
+		if (!oS.AutoSun) {
+			oSym.addTask(Math.round(1000 / oSym.TimeStep), ClickSun, [sunId]);
+		}
+	},
+}
+
 // oxlint-disable-next-line complexity
 document.addEventListener("keydown", (event) => {
 	// Ignore if typing in input fields
@@ -11,47 +24,19 @@ document.addEventListener("keydown", (event) => {
 	// Add key to sequence
 	keySequence += event.key.toLowerCase();
 
-	// Clear sequence after 2 seconds of inactivity
+	// Clear sequence after 5 seconds of inactivity
 	clearTimeout(sequenceTimeout);
 	sequenceTimeout = setTimeout(() => {
 		keySequence = "";
-	}, 2000);
+	}, 5000);
 
 	// Check for cheat codes
-	// super fast (sf)
-	if (keySequence.includes("sf")) {
-		CSpeed(1000, 10, 1000);
-		keySequence = "";
-	}
-	// balloon (bln)
-	else if (keySequence.includes("bln")) {
-		oP.Balloon();
-		keySequence = "";
-	}
-	// restart (r)
-	/*else if (keySequence.includes("r")) {
-		SelectModal(oS.Lvl);
-		keySequence = "";
-	}*/
-	// almanac (k)
-	else if (keySequence.includes("k")) {
-		ViewHandBook();
-		keySequence = "";
-	}
-	// sun (j)
-	else if (keySequence.includes("j")) {
-		const sunId = AppearSun($User.Mouse.x - 45, $User.Mouse.y + 45, 25, false);
-		if (!oS.AutoSun) {
-			oSym.addTask(Math.round(1000 / oSym.TimeStep), ClickSun, [sunId]);
+	for (i in cheatCodes) {
+		if (keySequence.includes(i)) {
+			cheatCodes[i]();
+			keySequence = "";
 		}
-		keySequence = "";
 	}
-	// oneko (cat)
-	/*else if (keySequence.includes("cat")) {
-		// remove .hidden from #oneko
-		document.getElementById("oneko").classList.remove("hidden");
-		keySequence = "";
-	}*/
 
 	// Prevent sequence from getting too long
 	if (keySequence.length > 20) {
