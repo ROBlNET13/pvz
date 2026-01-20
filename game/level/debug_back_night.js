50900a448b766ee938f64ba583bbf4e4be1e8fdd
Clay <clay@clay.rip>
2026-01-20 14:56:24 -0500

    add debug back night

---
+++ game/level/debug_back_night.js
@@ -0,0 +1,75 @@
+oS.Init(
+	{
+		PName: allPlantsArray(),
+		ZName: [
+			oZombie,
+			oZombie2,
+			oZombie3,
+			oConeheadZombie,
+			oBucketheadZombie,
+			oNewspaperZombie,
+			oJackinTheBoxZombie,
+			oFootballZombie,
+			oDancingZombie,
+			oScreenDoorZombie,
+			oBackupDancer,
+			oZomboni,
+			oPoleVaultingZombie,
+			oJY,
+			oEunZombie,
+			oSnorkelZombie,
+			oDuckyTubeZombie2,
+			oDuckyTubeZombie3,
+			oDuckyTubeZombie4,
+			oDuckyTubeZombie1,
+			oWJY,
+		],
+		PicArr: (function () {
+			return ["images/interface/background3.webp"];
+		})(),
+		backgroundImage: "images/interface/background3.webp",
+		CanSelectCard: 1,
+		LevelName: "debug_back_night",
+		LvlEName: "debug_back_night",
+		SunNum: 4500,
+		Coord: 2,
+		LF: [0, 1, 1, 2, 2, 1, 1],
+		StartGameMusic: "WateryGraves",
+		LargeWaveFlag: { 10: $("imgFlag3"), 20: $("imgFlag1") },
+	},
+	{
+		AZ: [
+			[oZombie, 1, 1],
+			[oZombie2, 1, 1],
+			[oZombie3, 1, 1],
+			[oConeheadZombie, 1, 1],
+			[oBucketheadZombie, 1, 1],
+			[oNewspaperZombie, 1, 1],
+			[oJackinTheBoxZombie, 1, 1],
+			[oFootballZombie, 1, 1],
+			[oDancingZombie, 1, 1],
+			[oScreenDoorZombie, 1, 1],
+			[oZomboni, 1, 1],
+			[oPoleVaultingZombie, 1, 1],
+			[oJY, 1, 1],
+			[oDuckyTubeZombie2, 1, 1],
+			[oDuckyTubeZombie3, 1, 1],
+			[oDuckyTubeZombie1, 1, 1],
+			[oDuckyTubeZombie4, 1, 1],
+			[oSnorkelZombie, 1, 1],
+			[oEunZombie, 1, 1],
+			[oWJY, 1, 20, [20]],
+		],
+		FlagNum: 20,
+		FlagToSumNum: { a1: [29, 32, 47], a2: [29, 32, 67] },
+		FlagToMonitor: { 9: [ShowLargeWave, 0], 19: [ShowFinalWave, 0] },
+		FlagToEnd() {
+			NewImg("imgSF", "images/interface/trophy.png", "left:260px;top:233px", EDAll, {
+				onclick() {
+					SelectModal(166);
+				},
+			});
+			NewImg("PointerUD", "images/interface/PointerDown.gif", "top:185px;left:676px", EDAll);
+		},
+	}
+);
