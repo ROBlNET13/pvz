c112fca1fe5a3a77962edb27e38f8524259e3ca4
Clay <clay@clay.rip>
2026-01-26 01:55:16 -0500

    some optimizations + easing functions for later

--- game/js/Cfunction.js
+++ game/js/Cfunction.js
@@ -1,4 +1,29 @@
 /* eslint-disable complexity */
+window.setInterval = function (delay, fn) {
+	var start = Date.now();
+	var data = {};
+	data.id = requestAnimationFrame(loop);
+
+	return data;
+
+	function loop() {
+		data.id = requestAnimationFrame(loop);
+
+		if (Date.now() - start >= delay) {
+			fn();
+			start = Date.now();
+		}
+	}
+};
+
+window.clearInterval = function (data) {
+	try {
+		cancelAnimationFrame(data.id);
+	} catch (e) {
+		console.warn("clearInterval failed:", e);
+	}
+};
+
 var $User = (function () {
 	const platform = navigator.platform;
 	const userAgent = navigator.userAgent;
@@ -14,14 +39,10 @@ var $User = (function () {
 	// Global Random Helper
 	$Random = isHTTP ? "#" : "?";
 
-	// Text setting helper based on browser capability
-	innerText = isIE
-		? function (element, text) {
-				element.innerText = text;
-			}
-		: function (element, text) {
-				element.textContent = text;
-			};
+	// Text setting helper
+	innerText = function (element, text) {
+		element.textContent = text;
+	};
 
 	// PNG fix for IE6
 	if (isIE6) {
@@ -33,7 +54,13 @@ var $User = (function () {
 
 	innerHTML = function (element, content) {
 		if (element) {
-			element.innerHTML = content;
+			// Use textContent for plain text, innerHTML only when HTML tags are present
+			if (/<[^>]*>/.test(content)) {
+				element.innerHTML = content;
+			} else {
+				console.log("innerHTML called with plain text, using textContent instead.");
+				element.textContent = content;
+			}
 		}
 	};
 
@@ -199,6 +226,9 @@ var oS = {
 		EDAll = $("dAll");
 		EDPZ = $("dPZ");
 		EDAlloffsetLeft = EDAll.offsetLeft;
+		window.addEventListener("resize", function () {
+			EDAlloffsetLeft = EDAll.offsetLeft;
+		});
 		EDNewAll = EDAll.cloneNode(true);
 		EDNewFlagMeter = $("dFlagMeter").cloneNode(true);
 		ESSunNum = $("sSunNum");
@@ -4229,3 +4259,163 @@ var CPlants = NewO({
 		b.PrivateDie(b);
 	},
 });
+
+var interpolate = function (start, end, amt, easingFunc) {
+	return start + (end - start) * (easingFunc ? easingFunc(amt) : amt);
+};
+
+var easeInSine = function (x) {
+	return 1 - Math.cos((x * Math.PI) / 2);
+};
+
+var easeOutSine = function (x) {
+	return Math.sin((x * Math.PI) / 2);
+};
+
+var easeInOutSine = function (x) {
+	return -(Math.cos(Math.PI * x) - 1) / 2;
+};
+
+var easeInQuad = function (x) {
+	return x * x;
+};
+
+var easeOutQuad = function (x) {
+	return 1 - (1 - x) * (1 - x);
+};
+
+var easeInOutQuad = function (x) {
+	return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
+};
+
+var easeInCubic = function (x) {
+	return x * x * x;
+};
+
+var easeOutCubic = function (x) {
+	return 1 - Math.pow(1 - x, 3);
+};
+
+var easeInOutCubic = function (x) {
+	return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+};
+
+var easeInQuart = function (x) {
+	return x * x * x * x;
+};
+
+var easeOutQuart = function (x) {
+	return 1 - Math.pow(1 - x, 4);
+};
+
+var easeInOutQuart = function (x) {
+	return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+};
+
+var easeInQuint = function (x) {
+	return x * x * x * x * x;
+};
+
+var easeOutQuint = function (x) {
+	return 1 - Math.pow(1 - x, 5);
+};
+
+var easeInOutQuint = function (x) {
+	return x < 0.5 ? 16 * x * x * x * x * x : 1 - Math.pow(-2 * x + 2, 5) / 2;
+};
+
+var easeInExpo = function (x) {
+	return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+};
+
+var easeOutExpo = function (x) {
+	return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+};
+
+var easeInOutExpo = function (x) {
+	// oxlint-disable-next-line no-nested-ternary
+	return x === 0
+		? 0
+		: // oxlint-disable-next-line no-nested-ternary
+			x === 1
+			? 1
+			: x < 0.5
+				? Math.pow(2, 20 * x - 10) / 2
+				: (2 - Math.pow(2, -20 * x + 10)) / 2;
+};
+
+var easeInCirc = function (x) {
+	return 1 - Math.sqrt(1 - Math.pow(x, 2));
+};
+
+var easeOutCirc = function (x) {
+	return Math.sqrt(1 - Math.pow(x - 1, 2));
+};
+
+var easeInOutCirc = function (x) {
+	return x < 0.5 ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2 : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+};
+
+var easeInBack = function (x) {
+	const c1 = 1.70158;
+	const c3 = c1 + 1;
+	return c3 * x * x * x - c1 * x * x;
+};
+
+var easeOutBack = function (x) {
+	const c1 = 1.70158;
+	const c3 = c1 + 1;
+	return 1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2);
+};
+
+var easeInOutBack = function (x) {
+	const c1 = 1.70158;
+	const c2 = c1 * 1.525;
+	return x < 0.5 ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+};
+
+var easeInElastic = function (x) {
+	const c4 = (2 * Math.PI) / 3;
+	// oxlint-disable-next-line no-nested-ternary
+	return x === 0 ? 0 : x === 1 ? 1 : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+};
+
+var easeOutElastic = function (x) {
+	const c4 = (2 * Math.PI) / 3;
+	// oxlint-disable-next-line no-nested-ternary
+	return x === 0 ? 0 : x === 1 ? 1 : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+};
+
+var easeInOutElastic = function (x) {
+	const c5 = (2 * Math.PI) / 4.5;
+	// oxlint-disable-next-line no-nested-ternary
+	return x === 0
+		? 0
+		: // oxlint-disable-next-line no-nested-ternary
+			x === 1
+			? 1
+			: x < 0.5
+				? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+				: (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+};
+
+var easeOutBounce = function (x) {
+	const n1 = 7.5625;
+	const d1 = 2.75;
+	if (x < 1 / d1) {
+		return n1 * x * x;
+	} else if (x < 2 / d1) {
+		return n1 * (x -= 1.5 / d1) * x + 0.75;
+	} else if (x < 2.5 / d1) {
+		return n1 * (x -= 2.25 / d1) * x + 0.9375;
+	}
+	return n1 * (x -= 2.625 / d1) * x + 0.984375;
+};
+
+var easeInBounce = function (x) {
+	return 1 - easeOutBounce(1 - x);
+};
+
+var easeInOutBounce = function (x) {
+	return x < 0.5 ? (1 - easeOutBounce(1 - 2 * x)) / 2 : (1 + easeOutBounce(2 * x - 1)) / 2;
+};
