db9de8fea2c5c1c4f0b87a4d82170b5ddf7c70ed
Clay <clay@clay.rip>
2026-02-06 18:36:48 -0500

    fix

--- game/menu/js/credits.js
+++ game/menu/js/credits.js
@@ -1,2 +1,6 @@
 PlaySound2("ZombiesOnYourLawn", true);
 AllAudioPaused();
+
+menuContainer.querySelector(".content").addEventListener("animationend", () => {
+	menuContainer.querySelector(".credits-finale").style.opacity = "1";
+});
